b48c2285c5e29b980a7fd2b25bef59ea7412e7d1
Dorian Thompson <thompsondorian2@gmail.com>
2023-05-21 20:21:38 -0400

    finish left side of intro section

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './Intro.css';
+import Github from '../../img/icons/Github.png';
+import LinkedIn from '../../img/icons/LinkedIn.png';
 
 const Intro = () => {
     return (
@@ -8,13 +10,20 @@ const Intro = () => {
                 <div className="intro-name">
                     <span>Hi! I'm</span>
                     <span>Dorian Thompson</span>
-                    <span>Frontend Developer with experience also with the backend, who enjoys creating and improving the user experience of a webpage</span>
+                    <span>Frontend Developer with backend experience, who enjoys creating and improving the user experience of a webpage</span>
                 </div>
                 <button className="button intro-button">
                     Hire Me
                 </button>
+                <div className="intro-icons">
+                    <a  target="_blank" href="https://github.com/dorianthompson">
+                        <img src={Github} alt="" />
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/dorian-thompson-459171b4/">
+                        <img src={LinkedIn} alt="" />   
+                    </a>
+                </div>
             </div>
-            
 
             <div className="intro-right">
                 Picture and icons
